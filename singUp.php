9ac52abbb27507838617dfa5810ade296bd5ce37
DominicTwo <sorequedom@gmail.com>
2025-05-28 01:19:52 -0600

    OCnfuguracion de login a la base de datos

--- singUp.php
+++ singUp.php
@@ -59,7 +59,7 @@ if($_SESSION['login']){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrate</title>
+    <title>Registro</title>
     <link rel="stylesheet" href="./src/css/normalize.css">
     <link rel="stylesheet" href="./src/css/utilities.css">
     <link rel="stylesheet" href="./src/css/formularios.css">
