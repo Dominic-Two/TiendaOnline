b52c06ebbcacca2a44dcabc8bc2e92239ec08db9
DominicTwo <sorequedom@gmail.com>
2025-05-28 00:11:17 -0600

    Funcion de carrito de compras

--- src/template/carrito.php
+++ src/template/carrito.php
@@ -1,9 +1,9 @@
 <section class="contCarrito" id="contCarrito">
     <div class="carritoProd">
         <img src="./src/img/close.svg" alt="Close" class="close">
-        <h2>Carrito de Compras</h2>
+        <h2>Carrito</h2>
         <div class="sidebar-content" id="lista-carrito"></div>
-        <a href="#" id="vaciarCarrito">Vaciar Carrito</a>
+        <a href="#" id="vaciarCarrito">Vaciar carrito</a>
         <a href="./pago.php">Proceder al pago</a>
     </div>
 </section>
